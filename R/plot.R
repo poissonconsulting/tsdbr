ddfc6416104404baa619672e65bcc7c7a3c5fa36
Joe Thorley <joe@poissonconsulting.ca>
2018-06-20 06:43:13 -0700

    just plot corrected

--- R/plot.R
+++ R/plot.R
@@ -1,11 +1,9 @@
 plot_station <- function(data) {
   gp <- ggplot2::ggplot(data = data,
                         ggplot2::aes_string(x = "DateTime", y = "Value")) +
-    ggplot2::geom_line(ggplot2::aes_string(
-      group = "Type", linetype = "Type", color = "Status"), alpha = 1/2) +
+    ggplot2::geom_line(ggplot2::aes_string(color = "Status")) +
     ggplot2::ggtitle(data$Station[1]) +
-    ggplot2::scale_color_manual(values = c("black", "blue", "red"), drop = FALSE) +
-    ggplot2::scale_linetype_manual(values = c("solid", "dotted"), drop = FALSE)
+    ggplot2::scale_color_manual(values = c("black", "blue", "red"), drop = FALSE)
   print(gp)
   NULL
 }
@@ -30,9 +28,7 @@ ts_plot_data <- function(data) {
   checkor(check_data(data, list(Recorded = c(1, NA))),
           check_data(data, list(Corrected = c(1, NA))))
   
-  if(missing_column(data, "Corrected")) {
-    data$Corrected <- data$Recorded
-  } else if (missing_column(data, "Recorded")) {
+  if (missing_column(data, "Recorded")) {
     data$Recorded <- data$Corrected
   }
   
@@ -40,15 +36,7 @@ ts_plot_data <- function(data) {
     data$Status <- ts_integer_to_status(1L)
   } else check_vector(data$Status, ordered(status_values(), status_values()))
   
-  recorded <- data
-  data$Value <- data$Corrected
-  data$Type <- "Corrected"
-  recorded$Value <- recorded$Recorded
-  recorded$Type <- "Recorded"
-  data <- rbind(data, recorded)
-  rm(recorded)
-  data$Type <- factor(data$Type, levels = c("Recorded", "Corrected"))
-  
+  data$Value <- data$Recorded
   data <- split(data, data["Station"], drop = TRUE)
   lapply(data, plot_station)
   invisible()
